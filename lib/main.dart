606cbfa7c9638454f648f161fb948241aea1af9a
islamrabi3 <islamrabie123@gmai.com>
2021-12-14 22:39:29 +0200

    new resuse methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,48 @@
 import 'package:bmi/bmi_screen.dart';
+import 'package:bmi/constance/const.dart';
+import 'package:bmi/cubit/cubit.dart';
+import 'package:bmi/cubit/states.dart';
+import 'package:bmi/shared/cache_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bloc/bloc.dart';
+import 'bloc_observer/observer.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await CacheHelper.init();
+  bool darkModeCache = CacheHelper.sharedPreferences!.getBool('isDark')!;
+  print(' cache value is  $darkModeCache');
+  Bloc.observer = MyBlocObserver();
+  runApp(MyApp(darkModeCache));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  bool? isDark;
 
+  MyApp(this.isDark);
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
+    return BlocProvider(
+      create: (context) => AppCubit()..changeLightTheme(fromShared: isDark),
+      child: BlocConsumer<AppCubit, AppStates>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Flutter Demo',
+            themeMode:
+                AppCubit.get(context).isDark ? ThemeMode.dark : ThemeMode.light,
+            darkTheme: darkThemeData,
+            theme: lightthemeData,
+            home: BmiScreen(),
+          );
+        },
+        listener: (context, state) {},
       ),
-      home: BmiScreen(),
     );
   }
 }
